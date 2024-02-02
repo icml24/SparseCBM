f55d892fe27ab7161e43a0769823af913f760bf2
icml24-anon <72404775+icml24@users.noreply.github.com>
2024-02-02 14:05:27 +0300

    Add files via upload

---
+++ bottlenecks/cbm.py
@@ -0,0 +1,65 @@
+import torch
+import transformers
+import torch.nn as nn
+from configs import *
+import torch.nn.functional as nn
+
+class SparseCBLmodel_for_logits(nn.Module):
+    """
+    Basic architecture: CLIP model + two layers: CBL and FC (head)
+    """
+    def __init__(self, num_concepts: int, num_classes: int, model_name: str="openai/clip-vit-base-patch32"):
+        super().__init__()
+        self.clip = transformers.CLIPModel.from_pretrained(model_name)
+        self.processor = transformers.CLIPProcessor.from_pretrained(model_name)
+        for param in self.clip.parameters():
+            param.requires_grad=False
+        self.cbl = nn.Linear(num_concepts, num_concepts, bias=False)
+        self.head = nn.Linear(num_concepts, num_classes, bias=False)
+        
+    def forward(self, **batch):
+        cbl_out = self.cbl(self.clip(**batch).logits_per_image)
+        return cbl_out, self.head(cbl_out)
+
+def contrastive_loss(logits, dim: int):
+    """
+    Contrastive loss, adapted from https://sachinruk.github.io/blog/2021-03-07-clip.html
+    """
+    neg_ce = torch.diag(F.log_softmax(logits, dim=dim))
+    return -neg_ce.mean()
+
+def criterion_cbl(similarity: torch.Tensor) -> torch.Tensor:
+    """
+    Args:
+        similarity: is equal to logits_per_image
+    """
+    caption_loss = contrastive_loss(similarity, dim=0)
+    image_loss = contrastive_loss(similarity, dim=1)
+    return (caption_loss + image_loss) / 2.0
+
+def gumbel_contrastive_loss(logits, hard: bool=False, dim: int=0):
+    """
+    Simple contrastive loss based on Gumbel-Softmax distribution
+    """
+    gumbel_softmax_samples = F.gumbel_softmax(logits, tau=1.0, dim=dim, hard=hard)
+    neg_ce = -torch.log(gumbel_softmax_samples)
+    return neg_ce.mean()
+
+def criterion_gumbel(similarity: torch.Tensor, hard: bool=False) -> torch.Tensor:
+    """
+    Self-supervised contrastive loss for CBL training
+    """
+    first_loss = gumbel_contrastive_loss(similarity, hard=hard, dim=0)
+    second_loss = gumbel_contrastive_loss(similarity, hard=hard, dim=1)
+    return (first_loss + second_loss) / 2.0
+
+def criterion_l1(model, l1_lambda=1e-3):
+    """
+    Implements L1 regularization.
+    During training should be scaled by len(concepts)
+    """
+    l1_loss = 0.0
+    for param in model.cbl.parameters():
+        l1_loss += torch.norm(param, p=1)
+        
+    return l1_loss * l1_lambda
